050afe42a90f6fbddd08c736fa25c73eed777327
Paul <gasmenp@gmail.com>
2025-05-16 12:03:06 +0800

    bundling

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -9,13 +9,14 @@ import SecondaryButton from '@/Components/SecondaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 
+import "vue3-easy-data-table/dist/style.css";
+import Vue3EasyDataTable from "vue3-easy-data-table";
+
 
 import { submitActions, showLoader, hideLoader } from '@/Utils/toastApiWrapper';
 
 const onShowModal = ref(false);
 const onModalText = ref("");
-const onLoadingSubmit = ref(false);
-const userCollection = ref([]);
 
 const formValues = ref({
     id: null,
@@ -25,35 +26,15 @@ const formValues = ref({
     address: '',
 });
 
-const validation = ({ values }) => {
-    const errors = {
-        name: [],
-        email: [],
-        phone: [],
-        address: [],
-    };
+const headers = [
+    { text: "Name", value: "name" },
+    { text: "Email", value: "email" },
+    { text: "Phone", value: "phone", sortable: true },
+    { text: "Address", value: "address", sortable: true },
+    { text: "", value: "action", width: 30 },
+];
 
-    if (!values.name) {
-        errors.name.push({ type: 'required', message: 'name is required.' });
-    }
-
-    if (!values.email) {
-        errors.email.push({ type: 'required', message: 'email is required.' });
-    }
-
-    if (!values.phone) {
-        errors.phone.push({ type: 'required', message: 'phone is required.' });
-    }
-
-    if (!values.address) {
-        errors.address.push({ type: 'required', message: 'address is required.' });
-    }
-
-    return {
-        values,
-        errors
-    };
-};
+const items = ref([]);
 
 const onShowModalUpdate = (data) => {
     onShowModal.value = true;
@@ -78,7 +59,7 @@ const fetchData = async () => {
 
     try {
         const response = await axios.get('api/userdetails/fetch');
-        userCollection.value = response.data;
+        items.value = response.data;
     } catch (error) {
         console.error('Error fetching users:', error);
     }
@@ -189,16 +170,14 @@ const onDelete = async (id) => {
 }
 
 onMounted(() => {
-    console.log('sdasdddd App.vue page mounted');
-     fetchData();
+    fetchData();
 });
 
 </script>
 
 <template>
 
-    <!-- <Head title="CRUD" /> -->
-    TANGINA MO
+    <Head title="CRUD" />
     <div class="text-center mt-10">
         <PrimaryButton label="Create User" @click="onShowModal = true; onModalText = 'Create User'">
             Create User
@@ -252,86 +231,24 @@ onMounted(() => {
             </div>
         </Modal>
 
-        <!-- <div class="card flex justify-center">
-            <Dialog v-model:visible="onShowModal" modal header="Edit Profile" :style="{ width: '25rem' }">
-                <template #header>
-                    <div class="inline-flex items-center justify-center gap-2">
-                        <span class="font-bold whitespace-nowrap">{{ onModalText }}</span>
-                    </div>
-                </template>
-
-
-<Form v-slot="$form" :formValues :resolver="validation" @submit="onFormSubmit">
-    <div class="flex flex-col gap-3">
-        <div class="flex flex-col gap-1">
-            <label for="name" class="font-semibold w-24">Name</label>
-            <InputText v-model="formValues.name" id="name"
-                :name="onModalText === 'Update User' ? (!formValues.name ? 'name' : formValues.name) : 'name'"
-                class="flex-auto" autocomplete="off" />
-            <span class="text-red-500">{{ $form.name?.error?.message }}</span>
-        </div>
-        <div class="flex flex-col gap-1">
-            <label for="email" class="font-semibold w-24">Email</label>
-            <InputText v-model="formValues.email" id="email"
-                :name="onModalText === 'Update User' ? (!formValues.email ? 'email' : formValues.email) : 'email'"
-                class="flex-auto" autocomplete="off" />
-            <span class="text-red-500">{{ $form.email?.error?.message }}</span>
-        </div>
-        <div class="flex flex-col gap-1">
-            <label for="phone" class="font-semibold w-24">Phone</label>
-            <InputText v-model="formValues.phone" id="phone"
-                :name="onModalText === 'Update User' ? (!formValues.phone ? 'phone' : formValues.phone) : 'phone'"
-                class="flex-auto" autocomplete="off" />
-            <span class="text-red-500">{{ $form.phone?.error?.message }}</span>
-        </div>
-        <div class="flex flex-col gap-1">
-            <label for="address" class="font-semibold w-24">Address</label>
-            <InputText v-model="formValues.address" id="address"
-                :name="onModalText === 'Update User' ? (!formValues.address ? 'address' : formValues.address) : 'address'"
-                class="flex-auto" autocomplete="off" />
-            <span class="text-red-500">{{ $form.address?.error?.message }}</span>
-        </div>
     </div>
 
-    <div class="flex justify-end mt-5">
-        <Button label="Cancel" text severity="secondary" @click="onCancelModal()" autofocus />
-        <Button type="submit" :label="onModalText === 'Update User' ? 'Update' : 'Save'" :loading="onLoadingSubmit"
-            severity="info" autofocus />
-    </div>
-</Form>
+    <div class="p-10">
+        <Vue3EasyDataTable :headers="headers" :items="items" alternating table-class-name="customize-table"
+            class="cursor-pointer">
 
-
-</Dialog>
-</div> -->
-    </div>
-
-    <!-- <div class="p-5 mt-10 text-center">
-        <DataTable :value="userCollection" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
-            tableStyle="min-width: 50rem"
-            paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-            currentPageReportTemplate="{first} to {last} of {totalRecords}">
-            <template #paginatorstart>
-                <Button type="button" icon="pi pi-refresh" text />
+            <template #item-action="item">
+                <div class="flex gap-1 p-2">
+                    <button @click="onShowModalUpdate(item)"
+                        class="bg-indigo-500 text-white rounded-lg px-3 py-1 ml-2 hover:bg-indigo-700 transition ease-in-out duration-150">
+                        Update
+                    </button>
+                    <button @click="onDelete(item.id)"
+                        class="bg-red-500 text-white rounded-lg px-3 py-1 ml-2 hover:bg-red-700 transition ease-in-out duration-150">
+                        Delete
+                    </button>
+                </div>
             </template>
-            <Column field="name" header="Name" style="width: 25%"></Column>
-            <Column field="email" header="Email" style="width: 25%"></Column>
-            <Column field="phone" header="Phone" style="width: 25%"></Column>
-            <Column field="address" header="Address" style="width: 25%"></Column>
-            <Column style="width: auto">
-                <template #body="{ data }">
-                    <div class="flex gap-2">
-                        <Button icon="pi pi-pen-to-square" @click="onShowModalUpdate(data)" severity="info"
-                            rounded></Button>
-                        <Button icon="pi pi-trash" @click="onDelete(data.id)" severity="danger" rounded></Button>
-                    </div>
-                </template>
-            </Column>
-
-            <template #empty> 
-                <span >No Users found.</span>
-            </template>
-        </DataTable>
-    </div> -->
-
-
+        </Vue3EasyDataTable>
+    </div>
 </template>
