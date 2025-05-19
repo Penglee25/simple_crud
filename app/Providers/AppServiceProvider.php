a4c795681fdc920cef744c6999faafd242c01a0b
Paul <gasmenp@gmail.com>
2025-05-19 15:49:12 +0800

    add file handling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,12 +5,17 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Storage;
 
 class AppServiceProvider extends ServiceProvider
 {
-    
+
     public function boot(): void
     {
+        if (!Storage::exists('public/photos')) {
+            Storage::makeDirectory('public/photos');
+        }
+
         Vite::prefetch(concurrency: 3);
         // Load API routes manually
         Route::prefix('api')
